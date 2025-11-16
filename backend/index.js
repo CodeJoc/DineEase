2edbfbefa610ff25bc29ccae38d8ee1d6dc9baa1
Harsh Doliya <doliyaharsh@gmail.com>
2025-11-16 15:57:42 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dine-ease-restaurant.vercel.app/",
     credentials: true,
   })
 );
